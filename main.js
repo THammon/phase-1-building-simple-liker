1f066e8f41ae839cf6e18b25ee58e8eed248575e
Tristen <tristen.hammon21@gmail.com>
2022-09-07 08:05:27 -0500

    commit tree 55d0282f0ae6f504e2fe4186007b6d77b4a86d79

--- main.js
+++ main.js
@@ -3,7 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// setTimeout(() => {
+//   console.log("3..2..1..");
+// },3000)
 
+// let likeButtons = document.querySelectorAll('.like')
+
+// function iLiked(event){
+//   let hearts = event.target = ("click", hearts);
+//   mimicServerCall("url")
+//     .then(() => {});
+    
+//     .catch(function(error) {
+//       alert ("Wrong.")
+//       document.getElementById = "modal".className = "";
+//     });
+// }
+const glyphObject = {'♡':FULL_HEART,'♥':EMPTY_HEART}
+const colorState = {"red":"","":"red"}
+///
+let likeButtons = document.querySelectorAll(".like")
+for (el of likeButtons){
+el.addEventListener("click", (e) => {
+mimicServerCall()
+.then((respond) => {
+e.target.innerText = glyphObject[e.target.innerText]
+e.target.style.color = colorState[e.target.style.color]
+})
+.catch((error) => {
+document.getElementById("modal").className = ""
+setTimeout(function() {
+document.getElementById("modal").className.hidden = "hidden"
+}, 3000);
+})
+})
+}
 
 
 
